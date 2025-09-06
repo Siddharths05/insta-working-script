aae61a272508606e27392eadf3c5f0b72aa52161
kqtqki <siddharthsurag05@gmail.com>
2025-09-06 19:26:21 +0530

    Changes to be committed: 	deleted:    test.js

--- test.js
+++
@@ -1,7 +0,0 @@
-import InstaTouch from "instatouch";
-
-const test = await InstaTouch.getUserMeta("instagram", {
-  session: "6831706502%3A5fjcrywrga0KqT%3A7%3AAYdFGA0mC8QJZvfhm92NmDkBI7L30QkUOydKDtsEsuk"
-});
-
-console.log(test.username, test.fullName);
